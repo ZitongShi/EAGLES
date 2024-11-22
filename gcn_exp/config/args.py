8f0e838486b9e9716cc11a872643685c846db5cd
Xixeen <18727475981@163.com>
2024-11-22 13:42:36 +0800

    update

--- gcn_exp/config/args.py
+++ gcn_exp/config/args.py
@@ -1,37 +1,27 @@
 import argparse
 
 def parser_loader():
-    parser = argparse.ArgumentParser(description='MoG(arxiv)')
-    # experiment settings
+    parser = argparse.ArgumentParser(description='GCNexp')
     parser.add_argument('--device', type=str, default='0')
     parser.add_argument('--log_steps', type=int, default=1)
     parser.add_argument('--temp_N',type=int,default=20)
-    parser.add_argument('--temp_r',type=float,default=1e-3)#0.001
+    parser.add_argument('--temp_r',type=float,default=1e-3)
     parser.add_argument('--seed',type=int,default=5)
-
-
-    # args about optim
     parser.add_argument('--lr', type=float, default=0.01)
     parser.add_argument('--weight_decay',type=float,default=1e-4)
-    
-    # args about gnn
     parser.add_argument('--hidden_channels',type=float,default=256)
     parser.add_argument('--num_layers', type=int, default=3)
-    parser.add_argument('--dropout', type=float, default=0.5)    
-    
-    # args about SpLearner expert
+    parser.add_argument('--dropout', type=float, default=0.5)
     parser.add_argument('--hidden_spl',type=float,default=128)
     parser.add_argument('--num_layers_spl',type=int,default=2)
-    
-    # args about MoE
     parser.add_argument('--expert_select',type=int,default=3)
     parser.add_argument('--k_list', nargs='+', type=float)
     parser.add_argument('--lam',type=float,default=1e-1)
     parser.add_argument('--use_topo',default=True,action="store_true")
     parser.add_argument('--dataset', type=str, default='Reddit',
                         help='Dataset',
-                        choices=['Cora', 'Citeseer', 'Pubmed', 'Flickr', 'ogbn-arxiv', 'Reddit', 'Reddit2',
-                                 'Yelp',"Cs","Physics","computers","photo",'ogbn-products','ogbn-proteins'])
+                        choices=['Cora',  'Pubmed', 'ogbn-arxiv',
+                                 'ogbn-products'])
     parser.add_argument('--overlapping_rate', type=float, default=0.0, choices=[0.0,0.1,0.2,0.3,0.4,0.5],
                         help="Additional samples of overlapping data")
     parser.add_argument("--num_workers", type=int, default=10, help="number of clients")
@@ -48,8 +38,8 @@ def parser_loader():
     parser.add_argument('--selected_pruning_rate', type=int, default=30,help='Whether to fix the para')
     parser.add_argument('--agg_method', type=str, default="FedAvg",
                         help='Federated Algorithms')
-    parser.add_argument('--num_clusters', type=int, default=5, help='Number of clusters for FedCP')
-    parser.add_argument('--lambda2', type=float, default=0.1, help='Number of clusters for FedCP')
+    parser.add_argument('--num_clusters', type=int, default=5, help='')
+    parser.add_argument('--lambda2', type=float, default=0.1, help='')
 
 
 
